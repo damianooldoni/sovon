244e15c4b47dcbb426cc8586bae2b801592875af
damianooldoni <damiano.oldoni@inbo.be>
2018-06-20 16:21:13 +0200

    add function for connection to db

---
+++ R/connection_to_kleurring.R
@@ -0,0 +1,25 @@
+#' @description Provide connection to the INBO kleurringdatabank (the so called
+#'   *meeuwen* database). Valid only if rights are provided by INBO databeheer
+#'   team.
+#'
+#' @return conn ODBC connection to kleurring database.
+#'
+#' @export
+#'
+#' @importFrom DBI dbConnect
+#' @importFrom odbc odbc
+connect_to_kleurring <-function() {
+  conn <- DBI::dbConnect(odbc::odbc(),
+                         driver = driver,
+                         server = server,
+                         database = database,
+                         port = port,
+                         trusted_connection = "YES")
+  return(conn)
+}
+
+#' kleurring database informations
+driver <- "SQL Server Native Client 11.0"
+server <- "172.31.11.67"
+database <- "D0016_00_Meeuwen"
+port <- 1433
