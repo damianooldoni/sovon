2a45d6910dd7badf09e50e1c97b175ca860a6dd8
damianooldoni <damiano.oldoni@inbo.be>
2018-06-20 17:10:01 +0200

    Improve documentation get_users()

--- R/get_users.R
+++ R/get_users.R
@@ -4,12 +4,19 @@
 #'
 #' @param conn A valid connection to INBO kleurring database.
 #'
-#' @return A data.frame.
+#' @return A tibble data frame.
 #'
 #' @export
 #'
 #' @importFrom DBI dbGetQuery
 #' @importFrom dplyr %>% as_tibble
+#' @example
+#' \dontrun{
+#' conn <- connect_to_kleurring()
+#'
+#' # all users
+#' users <- sovon::get_users(conn)
+#' }
 get_users <- function(conn) {
   users <-  dbGetQuery(conn, "SELECT * FROM dbo.tblWaarnemer") %>%
     as_tibble()
